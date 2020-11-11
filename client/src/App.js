5bc4409e3f8f41db2b06c1dad88224470ade69e9
Nikola Stevanovic <jonnystevanovic@gmail.com>
2020-11-11 21:44:43 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
+import "./App.css";
+
+import { AuthProvider } from './context/auth';
+import AuthRoute from './utils/authRoute';
+
+import MenuBar from './components/MenuBar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import SinglePost from './components/SinglePost';
+
+function App() {
+    return(
+        <AuthProvider>
+            <Router>
+                <Container>
+                    <MenuBar />
+                    <Route exact path='/' component={Home} />
+                    <AuthRoute exact path='/login' component={Login} />
+                    <AuthRoute exact path='/register' component={Register} />
+                    <Route exact path='/posts/:postId' component={SinglePost} />
+                </Container>
+            </Router>
+        </AuthProvider>
+    );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typedefs');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req }),
+});
+
+mongoose
+    .connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        return server.listen({port: 5000});
+    })
+    .then(res => {
+        console.log(`Server running at: ${res.url}`);
+    });
+
+
+   
